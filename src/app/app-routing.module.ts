7db6f872f43bd43ae035c4f303cc30073353f28d
German Alexis <galvarez@abatechnology.com>
2023-11-08 23:11:14 -0500

    added all routering - added modules - improve style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/pages/home/home.component';
+import { AboutComponent } from './shared/pages/about/about.component';
+import { ContactComponent } from './shared/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-to', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const SHARED_COMPONENT = [SidebarComponent, HomeComponent];
-
+const SHARED_COMPONENT = [
+  SidebarComponent,
+  HomeComponent,
+  AboutComponent,
+  ContactComponent,
+];
 @NgModule({
   declarations: [...SHARED_COMPONENT],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [...SHARED_COMPONENT],
 })
 export class SharedModule {}
