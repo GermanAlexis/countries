3631a8398679cda145ea769bc4d05dcec955849d
German Alexis <alexisdager93@hotmail.com>
2023-11-25 11:11:57 -0500

    fix: refactor region

--- src/app/countries/pages/by-region-pages/by-region-pages.component.ts
+++ src/app/countries/pages/by-region-pages/by-region-pages.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/countries.interfaces';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Americas' | 'Europe' | 'Africa' | 'Oceania' | 'Asia';
+
 @Component({
   selector: 'app-by-region-pages',
   templateUrl: './by-region-pages.component.html',
@@ -10,10 +12,13 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPagesComponent {
   countries: Country[] = [];
   isLoading: boolean = false;
+  regions?: Region[] = ['Americas', 'Europe', 'Africa', 'Oceania', 'Asia'];
+  selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  onSearchByRegion(region: string) {
+  onSearchByRegion(region: Region) {
+    this.selectedRegion = region;
     this.countriesService.searchRegion(region).subscribe((country) => {
       this.isLoading = true;
       this.countries = country;
